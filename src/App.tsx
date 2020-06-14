3b4fa6cade74b5eb55f3263950653040cd015401
Kit Jonathan Te <kjonathante@gmail.com>
2020-06-13 22:54:12 -0700

    added aws-appsync.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import AWSAppSyncClient from "aws-appsync";
+import { AuthOptions } from "aws-appsync-auth-link";
+import { ApolloProvider, Query, QueryResult } from "react-apollo";
+import { Rehydrated } from "aws-appsync-react";
+import gql from "graphql-tag";
+
+import appSyncConfig from "./aws-exports";
+
+const client = new AWSAppSyncClient({
+  url: appSyncConfig.aws_appsync_graphqlEndpoint,
+  region: appSyncConfig.aws_appsync_region,
+  auth: {
+    type: appSyncConfig.aws_appsync_authenticationType,
+    apiKey: appSyncConfig.aws_appsync_apiKey,
+  } as AuthOptions,
+});
+
+const query = gql(`
+  query {
+    listEvents(limit: 1000) {
+      items {
+        id
+        name
+        where
+        when
+        description
+        comments {
+          items {
+            commentId
+          }
+        }
+      }
+    }
+  }
+`);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Query query={query}>
+      {({ loading, error, data }: QueryResult) => {
+        if (loading) {
+          return <div>Loading ...</div>;
+        }
+
+        if (error) {
+          return <div>Error ...</div>;
+        }
+
+        return (
+          <ul>
+            {data.listEvents.items.map((Event: { id: string }) => (
+              <li key={Event.id}>{Event.id}</li>
+            ))}
+          </ul>
+        );
+      }}
+    </Query>
   );
 }
 
-export default App;
+export default () => (
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <App />
+    </Rehydrated>
+  </ApolloProvider>
+);
